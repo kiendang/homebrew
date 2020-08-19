dcbf320ed49fa48f086221d0a3233de409401469
Kien Dang <mail@kien.ai>
2020-08-19 14:59:16 +0800

    nhaccuatui v2.0.0

--- Casks/nhaccuatui.rb
+++ Casks/nhaccuatui.rb
@@ -1,18 +1,19 @@
-cask 'nhaccuatui' do
-  version '1.0.2'
-  sha256 '26ceb0085d3ad083be827408de11a3ebf54e3b7e07ec21e35e0169b3254556bb'
+cask "nhaccuatui" do
+  version "2.0.0"
+  sha256 "2f0e01465a67c5d9814532b51e2302363d835bfc5a4886089acd7ef3cb49532e"
 
   url "http://apps.nhaccuatui.com/download/NhacCuaTui-#{version}.dmg"
-  name 'NhacCuaTui'
-  homepage 'https://www.nhaccuatui.com/'
+  name "NhacCuaTui"
+  homepage "https://www.nhaccuatui.com/"
 
-  app 'NhacCuaTui.app'
+  app "NhacCuaTui.app"
 
   zap trash: [
-               '~/Library/Application Support/NhacCuaTui',
-               '~/Library/Logs/NhacCuaTui',
-               '~/Library/Preferences/com.nhaccuatui.macos.helper.plist',
-               '~/Library/Preferences/com.nhaccuatui.macos.plist',
-               '~/Library/Saved Application State/com.nhaccuatui.macos.savedState',
-             ]
+    "~/Library/Application Support/NhacCuaTui",
+    "~/Library/Logs/NhacCuaTui",
+    "~/Library/Preferences/com.nhaccuatui.macos.helper.plist",
+    "~/Library/Preferences/com.nhaccuatui.macos.plist",
+    "~/Library/Preferences/vn.nhaccuatui.macos.plist",
+    "~/Library/Saved Application State/com.nhaccuatui.macos.savedState",
+  ]
 end
