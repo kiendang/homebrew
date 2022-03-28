0639b8fd32542e54de08bd2dd6c39ad22bcbf799
Kien Dang <mail@kien.ai>
2022-03-28 16:07:51 +0800

    Add Spectre

---
+++ Formula/spectre-cli.rb
@@ -0,0 +1,15 @@
+class SpectreCli < Formula
+  desc "Password cipher"
+  homepage "https://spectre.app/"
+  license "GPL-3.0-or-later"
+  head "https://gitlab.com/spectre.app/cli.git", branch: "main"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "./build"
+    bin.install "spectre"
+    bash_completion.install "spectre.completion.bash" => "spectre"
+  end
+end
